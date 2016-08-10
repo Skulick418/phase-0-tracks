5d4c7cfe544c136afdf49d7ba9a49bba46382269
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-10 11:01:12 -0400

    eliminate extra step in code update putting infro into hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,9 +1,22 @@
-client_details = {
-	:name => "",
-	:age => "",
-	:children_number => "",
-	:decor_theme => "",
-	:fish_tank => "",
-	:size_of_pool => "",
-	:dog => "",
-}
+#Create new hash
+client_form = {}
+
+#Driver code
+puts "Please enter client's name."
+client_form[:name_input] = gets.chomp #Made user input new data in hash's array.
+
+puts "Please enter the client's age."
+client_form[:age_input] = gets.chomp.to_i
+
+puts "Please enter the chosen theme"
+client_form[:theme_input] = gets.chomp
+
+puts "Please enter if fish tank will be installed (y/n)"
+client_form[:fish_input] = gets.chomp
+if :fish_input == "y"
+	:fish_input = true
+elsif :fish_input == "n"
+	:fish_input = false
+else
+	puts "please enter y or n case senesitive."
+end
