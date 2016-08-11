ca0f2c43462824772cb3f23f16d6520bbf527121
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-11 12:16:08 -0400

    fix bugs change name from class to school_room because of syntax error

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,25 +1,41 @@
-class__room = {
+School_room = {
 	'Chalk Board' => [
 		'chalk',
 		'eraser',
 		'sponge'
-	]
+	],
 
 	'Desk 1' => [
-		'pencil'.
+		'pencil',
 		'gum',
-		'caluclator'
-		'baseball cards'
+		'caluclator',
+		'baseball cards',
 		'cheat sheet'
-	]
+	],
 
 	'Desk 2' => [
-		'pen'
-		'candy'
-		'paper airplane'
-		'cell phone'
+		'pen',
+		'candy',
+		'paper airplane',
+		'cell phone',
 		'old homework'
+	],
+
+	'Desk 3' => [
+		'pencil',
+		'pen',
+		'flashlight',
+		'notes',
+		'wallet'
+	],
+	'Teachers Desk' => [
+		'grades',
+		'lap top',
+		'notebook',
+		'red pen'
 	]
 
-	
+
 }
+
+School_room['Teachers Desk'][2]
