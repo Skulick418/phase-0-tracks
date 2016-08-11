6cf61f9cdc11d6747d2f1b0e2da971e265a68568
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-11 12:09:32 -0400

    Initial commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,25 @@
+class__room = {
+	'Chalk Board' => [
+		'chalk',
+		'eraser',
+		'sponge'
+	]
+
+	'Desk 1' => [
+		'pencil'.
+		'gum',
+		'caluclator'
+		'baseball cards'
+		'cheat sheet'
+	]
+
+	'Desk 2' => [
+		'pen'
+		'candy'
+		'paper airplane'
+		'cell phone'
+		'old homework'
+	]
+
+	
+}
