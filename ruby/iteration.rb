87d7754cbbd6f933d28bcedbf92fd1c4f5872e7a
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-11 11:47:04 -0400

    Add calls and proper syntex to make code work for calls, final draft commit

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -46,7 +46,7 @@ number.delete_if { |digits| digits < 3 }
 
 p number 
 
-#hash 1
+# hash 1
 
 
 sports = {"football" => 1, "soccer" => 2, "baseball" => 3, "volleyball" => 4}
@@ -55,40 +55,57 @@ sports.delete_if {|key, value| key == "football" }
 
 p sports 
 
+# array 2
+
 numbers = [1,2,3,4,5,6,7,8,9,10]
 
 numbers.keep_if { |num| num > 5}
 
+p numbers
+
+# hash 2
+
+final_score = { 'football' => 23, 'soccer' => 2, 'volleyball' => 24, 'baseball' => "Red Sox 4 Mets 3"}
+
+final_score.keep_if {|key, value| key == "baseball" }
+
+p final_score
 
 
-final_score = { 'football' => 23, 'soccer' => 2, 'volleyball' => 24, 'baseball' => 4}
-
-final_score.keep_if {|key, value| key == 'football' }
-
-
-
+# array 3
  
 alphabet = ['a', 'b', 'c', 'd', 'e']
 
 new_alphabet = alphabet.select { |char| char =~ /[abc]/ }
 
-hash
+p new_alphabet
+
+# hash 3
 
 random_data = {'a' => 123, 'b'=> 345, 'c' => 369, 'd' => 554}
 
-random_data.select { |key, value| value < 350}
+new_data = random_data.select { |key, value| value < 350}
+
+p new_data
 
 
 
 
 
-
+# array 4
 
 color = ['blue', 'yellow', 'green', 'white']
 
-color.take_while { |item| item.length < 10}
+new_color = color.take_while { |item| item.length < 6 }
+
+p new_color
+
+ 
 
 
 bird_population = { 'parrot' => 4, 'pigeon' => 23, "crow" => 10, "flamingo" => 45 }
 
-bird_population.reject { |key, value| value < 44 }
+ new_pop = bird_population.reject { |key, value| value < 44 }
+ 
+ p new_pop 
+
