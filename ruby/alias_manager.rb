4db6ed832aba44d89c32391e3c0305b1ca579396
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-11 14:44:14 -0400

    .

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,24 @@
-name ="Felicia Torres"
-names = name.split
-firstname = names.first
-lastname = names.last 
-p "#{lastname} #{firstname}"
+do |user_name|
+
+
+def encrypt (user_name)
+	user_name_length = user_name.length
+	i = 0
+	user_name_out = ""
+	while i < user_name.length
+		if user_name == "z"
+			user_name_out += "a"
+			i += 1
+		else
+			user_name_out += user_name[i].next
+			i += 1
+		end
+	end
+	puts user_name_out
+end
+
+
+
+
+
+
