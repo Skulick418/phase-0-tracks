6d4ac457b224f8396fda4fefdbebffd91f22771f
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-11 15:11:17 -0400

    Add hash example

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,9 +1,10 @@
-School_room = {
+]School_room = {
 	'Chalk Board' => [
 		'chalk',
 		'eraser',
 		'sponge'
 	],
+
 
 	'Desk 1' => [
 		'pencil',
@@ -42,3 +43,40 @@ puts School_room['Teachers Desk'][2]
 puts School_room['Desk 3'][0]
 School_room['Teachers Desk'].push('Student note')
 puts School_room['Teachers Desk']
+
+
+snowboard_types = { 
+	All_terain: {
+		Burton: {
+			Size: ['normal', 'long'],
+			Goofy_option: 'yes',
+			Sale: 'no'
+		},
+		Forum: {
+			Size: ['normal', 'long'],
+			Goofy_option: 'no',
+			Sale: 'yes'
+		}
+	},
+	Freestyle: {
+		Burton: {
+			Size: ['normal', 'long'],
+			Goofy_option: 'yes',
+			Sale: 'yes'
+		},
+		Forum: {Size: ['normal', 'long'],
+			Goofy_option: 'no',
+			Sale: 'yes'
+		}
+	}
+
+
+
+}
+	
+
+
+
+puts snowboard_types[:Freestyle][:Burton][:Size][0..1]
+puts snowboard_types[:All_terain][:Forum][:Goofy_option]
+puts snowboard_types[:Freestyle][:Burton][:Sale]
