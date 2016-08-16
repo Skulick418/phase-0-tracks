5acc2f42a0e5d799067ed2da95549f4227422260
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-16 14:15:46 -0400

    finish release 1, add puppy class with various methods, test methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,34 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def bark(x) 
+    x.to_i.times {puts "woof"} 
+  end 
+  
+  def roll_over 
+    puts "roll over"
+  end 
+  
+  def dog_years(x)
+    age_conversion = x.to_i * 7 
+    puts "#{x} is #{age_conversion} in dog years"
+  end 
+  
+  def friend(dog_name)
+    puts "#{dog_name} is my friend"
+end 
+end 
 
-end
 
+
+dax = Puppy.new 
+dax.fetch('ball')
+
+dax.bark(5)
+
+dax.roll_over
+
+dax.dog_years(2)
+
+dax.friend('spot')
