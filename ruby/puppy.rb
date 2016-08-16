2d9b7edabf0e12e251a97cb1525e4649143eb8fe
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-16 10:15:53 -0400

    Create fake case class for puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+# *Species 
+# 	-Welsh Terrier 
+# *Characteristics 
+# 	Energy level: High  
+#  	Good with kids: Yes 
+#   Sheds: No 
+#   Color: Black and Tan 
+#   Size: Medium 
+# *Behavior 
+# 	Digs: Yes 
+# 	Barks: Minimal 
+#   Learn Tricks: Yes 
