cbf1a0bbae1914c89691821a543bd5ba1d98e949
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-19 17:55:33 -0400

    Finish draft of case, adding userinterface loop

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,10 +1,7 @@
-#Design a class that allows user to build a snowboard. 
-
-
 class Snowboard_Build 
 	attr_reader :name, :height, :company   
 	attr_accessor :front_foot, :style
-	def initialize(name, height, style, front_foot, company)
+	def initialize(name, height, style, left_foot, company)
 		puts "Begin initilization for snowboard builder"
 		@name = name 
 		@height = height
@@ -12,6 +9,7 @@ class Snowboard_Build
 		@company = company
 		@bindings = 2 
 		@flex = "middle"
+		@left_foot = left_foot 
 	end 
 
 def description
@@ -19,14 +17,38 @@ def description
 	puts "Height" + @height
 	puts "Style" + @style 
 	puts "Company" + @company 
-	puts "Front Foot" + @front_foot
+	puts "Left Foot" + @left_foot
 	puts "Bindings" + @bindings
 	puts "Board Flex" + @flex 
 	puts "New board has been created"
+	if @left_foot = true 
+		puts "#{@name} needs the left binding infront"
+	elsif @left_foot = false 
+		puts "#{@name} needs the right binding infront"
+	end
 end 
+
+def get_some_air_question
+	puts "Are you going to go for the jump?"
+	answer = gets.chomp.downcase
+		if answer == "yes"
+			puts "Enjoy the air!"
+		else 
+			puts "Go big or go home!"
+		end
+end 
+def front_foot
+	if @left_foot 
+		@left_foot = true 
+	else
+		@left_foot = false
+	end
+end
+
+
 end 
 
-test_board = Snowboard_Build.new("Sam", "6'3", "Park", "Right", "Burton")
+# p test_board = Snowboard_Build.new("Sam", "6'3", "Park", "Right", "Burton")
 
-
+# test_board.get_some_air_question
 
