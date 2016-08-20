b601a23bcb43fb53253755d2ad24ede9ef6df1ef
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-20 09:41:51 -0400

    Begin UI

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,6 +1,6 @@
 class Snowboard_Build 
-	attr_reader :name, :height, :company   
-	attr_accessor :front_foot, :style
+	attr_reader :bindings,:flex   
+	attr_accessor :front_foot, :style,:name, :height, :company
 	def initialize(name, height, style, left_foot, company)
 		puts "Begin initilization for snowboard builder"
 		@name = name 
@@ -44,11 +44,33 @@ def front_foot
 		@left_foot = false
 	end
 end
-
-
 end 
 
 # p test_board = Snowboard_Build.new("Sam", "6'3", "Park", "Right", "Burton")
 
 # test_board.get_some_air_question
+
+loop do 
+	puts "Hello what is your name? (type quit to enter)."
+	name_or_break = gets.chomp.downcase
+	break if name_or_break == "quit"
+	snowboard_form = []
+	snowboard_form[:name] = name_or_break
+
+	puts "Please enter your height"
+	height = gets.chomp
+	snowboard_form[:height] = height 
+
+	puts "Please pick your riding style. Choices: Park, Downhill."
+	style_choice = gets.chomp.downcase 
+		if style_choice == "park"
+			snowboard_form[:style] = style_choice
+		elsif 
+			style_choice == "downhill"
+			snowboard_form[:style] = style_choice
+		else 
+			snowboard_form[:style] = "Invalid choice"
+		end 
+	puts "Please pick which company of board you would prefer. (Burton, Forum, Ride)."
+	board_choice = gets.chomp.downcase
 
