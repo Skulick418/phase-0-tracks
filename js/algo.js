4634f92075cce5139f9bb94670fdcce01e5b9637
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-28 14:52:34 -0400

    Psuedocode initial commit

---
+++ js/algo.js
@@ -0,0 +1,8 @@
+//PSUEDOCODE
+// RELEASE 0: 
+		//Create a function that can take two objects and check if the objects share at least one key-value pair. 
+				// Make a function that can take two parameters. 
+				// loop through both parameters 
+						// if statement if the variable of object one is equal to teh property and value of object two
+						// create a variable called does_it_match and set it as true. 
+								// Create if statement 
