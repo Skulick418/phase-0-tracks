995a9045425c78c626810cec3c8e8e54afbf9658
Sam Kulick <sam_kulick@my.uri.edu>
2016-08-28 13:03:07 -0400

    Write code tested in repl

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,18 @@
 // "hello" => "olleh"
 
 //Declare a function and set the string that will be reversed as the argument
+	// set a variable to put the the parameter 
+	// set up a loop using for to run over each character in the string
+	// loop will look like var i = (parameter).length-1; i >= 0; i--
+	// store new character in empty string variable 
+// loops repeat until completed 
+// returns the new string 
+
+function reverse(string) {
+	var new_string = "";
+	for (var i = string.length-1; i >= 0; i--) {
+		new_string += string[i]
+	}
+	return new_string
+}
+
