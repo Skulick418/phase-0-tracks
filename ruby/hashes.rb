6cc9393151241212ecd549bf7ef8d569bf18c345
Sam Kulick <sam_kulick@my.uri.edu>
2016-09-03 12:14:07 -0400

    Wrote pseudocode began initial code running an initial commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+#PSEUDOCODE
+# Creating a program that asks what the input is, inserts that input into a hash, and then allows for updates.
+	#Ask for user information, set userinformation as value for key variable we will create
+		# gets.chomp will be equal to hash_name[:key_name]
+	#Create loop that will break if the answer returned is done.
+		#Otherwise the user will put in a keyword that will go to a key that will lead to an update.
+			# Have new answer be equal to the value of the existing key 
+	# End program
+
+puts "Welcome to the interior design manipulator 3000!"
+puts "Please, let's begin...."
+
+puts "Please put in client's full name (first name, last name)"
+name_input = gets.chomp.to_s 
+
+
