e6851380723ac75de225975b55e3898ef9a0a7b4
Sam Kulick <sam_kulick@my.uri.edu>
2016-09-03 14:20:51 -0400

    final commit

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -58,3 +58,35 @@ p sports
  sports.keep_if {|key, value| value < 3 }
 
  p sports 
+
+ #Array 3 
+
+alphabet = ['a', 'b', 'c', 'd', 'e']
+
+new_alphabet = alphabet.select { |char| char =~ /[abc]/ }
+
+p new_alphabet
+
+# Hash 3 
+
+random_data = {'a' => 123, 'b'=> 345, 'c' => 369, 'd' => 554}
+
+new_data = random_data.select { |key, value| value < 350}
+
+p new_data
+
+# Array 4
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+new_numbers = numbers.take_while {|x| x < 4}
+other_numbers = numbers.drop_while {|x| x < 4}
+
+p new_numbers
+p other_numbers
+
+# Hash 4 
+
+menu_item = {"chicken" => 1, "salad" => 2,"steak" => 3}
+new_menu = menu_item.reject { |key, value| key == "salad" }
+p new_menu
