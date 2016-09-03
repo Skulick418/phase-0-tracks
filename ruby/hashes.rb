b9d9300eb228dc81306941282df80d2e159090a6
Sam Kulick <sam_kulick@my.uri.edu>
2016-09-03 12:19:46 -0400

    Add boolean question add questions that are required

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,10 +7,28 @@
 			# Have new answer be equal to the value of the existing key 
 	# End program
 
+list = Hash.new 
+
 puts "Welcome to the interior design manipulator 3000!"
 puts "Please, let's begin...."
 
-puts "Please put in client's full name (first name, last name)"
-name_input = gets.chomp.to_s 
+puts "Please tell me the client's full name (first name, last name)"
+list[:name] = gets.chomp.to_s 
+
+puts "Please tell me the client's age"
+list[:age] = gets.chomp.to_i 
+
+puts "Please tell me the amount of children the client has"
+list[:child_amount] = gets.chomp.to_i
+
+puts "Please tell me if a fish tank will be added (yes/no)"
+fish_tank_input = gets.chomp.downcase 
+	if fish_tank_input == "yes"
+		list[:fish_tank] = true 
+	elsif fish_tank_input == "no"
+		list[:fish_tank] = false 
+	else 
+		puts "Please type in yes or no."
+	end 
 
 
