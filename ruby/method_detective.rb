db2e579733ffdac5b219dbd6c71922f40400aff7
Sam Kulick <sam_kulick@my.uri.edu>
2016-09-03 12:02:30 -0400

    Finish with all the method signature calls

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -19,21 +19,21 @@ p "enhance".center(15)
 p "the usual".replace("the usual suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".replace("the usual suspects")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+p "The mystery of the missing first letter".replace("he Mystery of the missing first letter") 
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+p "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+p "z".ord
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (122 is the integer ordinal for z)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".index("a")
 # => 4
