22441a1e99ecb408b80b7a6db5108c6cdae76a59
Sam Kulick <sam_kulick@my.uri.edu>
2016-09-03 14:01:32 -0400

    finish release one

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,20 @@ def talk
 	yield ("Kulick")
 end
 talk {|name| puts name}
+
+fruits = ["apple", "pear", "watermelon","cherry"]
+fruit_salad = ["grapefruit", "raisins"]
+
+fruits.each do |x|
+	fruit_salad << x
+end 
+
+p fruit_salad
+
+
+fruits.map! do |x|
+	puts "I like to eat #{x}'s"
+end
+	
+end 
+p fruit_salad
