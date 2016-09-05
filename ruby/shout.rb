fa961cc9b3b4e9d8702a4e8a0941ad59a7fa1dcb
Sam Kulick <sam_kulick@my.uri.edu>
2016-09-05 16:03:42 -0400

    Fix up previous version of module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,18 +1,42 @@
+# module Shout 
+# 	puts "Hello world!"
+
+# 	def self.yell_angrily(words)
+#     	words + "!!!" + " :("
+#   	end
+
+#   	def self.yelling_happily(words)
+#   		words + "!!!" + " :) YAY!"
+# 	end
+# end 
+
+# p Shout.yelling_happily("Sam")
+# p Shout.yell_angrily("Sam")
+
 module Shout 
-	puts "Hello world!"
-
-	def self.yell_angrily(words)
-    	words + "!!!" + " :("
-  	end
-
-  	def self.yelling_happily(words)
-  		words + "!!!" + " :) YAY!"
+	def hockey_fan(team)
+		puts "#{team} are the best!"
 	end
+end
+
+class Islanders 
+	include Shout 
 end 
 
-p Shout.yelling_happily("Sam")
-p Shout.yell_angrily("Sam")
+class Rangers
+	include Shout
+end 
 
+#Driver Code# 
+
+isles = Islanders.new 
+isles.hockey_fan("Islanders")
+
+rags = Rangers.new 
+rags.hockey_fan("Somehow I think the Rangers")
+
+
+module 
 # module Shout
 #   puts "ECHO ECHO ECHO"
 
